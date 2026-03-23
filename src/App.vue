f4269e8fe58506804f8e9f9ca232e8c81e94969d
El-cacahu3te <elia.nicolo@outlook.com>
2026-03-23 19:30:23 +0100

    équivalence kalvin,celcius, etc, encore problème avec router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './pages/Home.vue'
+import Temperature from './pages/Temperature.vue'
+
+import { useHashRoute } from './composables/useHashRoute.js'  
+
+
+
+const {curentPage} = useHashRoute();
+console.log(curentPage.value);
+const router = [
+  {name : 'home', component : Home},
+  {name : 'temperature', component : Temperature}
+]
 </script>
 
 <template>
-  <HelloWorld />
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#temperature">Temperature</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <Home v-if="curentPage === 'home'"/>
+   <!-- affiche la page que si la conditione est vraie -->
+    <Temperature v-if="curentPage === 'temperature'"/>
+  </main>
 </template>
